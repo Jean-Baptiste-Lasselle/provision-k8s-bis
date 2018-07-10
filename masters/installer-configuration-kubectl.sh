16e98c68949d69d1f3cece14f11cb3291f0cbfe1
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-07-10 08:52:52 +0200

    je ne fais plus la configuration [kubectl] que pour que l'utilisateur courant puisse exécuter [kubectl] avec sudo

--- masters/installer-configuration-kubectl.sh
+++ masters/installer-configuration-kubectl.sh
@@ -4,25 +4,28 @@
 #     "root" 
 export UTILISATEUR_DE_DEPART=$USER
 
-# - CONFIGURATION KUBECTL pour "root"
-sudo -s
+# - CONFIGURATION KUBECTL pour "root"  / ça pose porblème, je reste en root, et je ne veux pas toucher aux ugo pour l'instant. Donc je ne le fais pas.
+# sudo -s
 # - on fait cela en tant que "root"
-rm -rf $HOME/.kube
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+# rm -rf $HOME/.kube
+# mkdir -p $HOME/.kube
+# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# chown $(id -u):$(id -g) $HOME/.kube/config
 
 # - CONFIGURATION KUBECTL pour "$UTILISATEUR_DE_DEPART"
 # - on redonne la main à l' $UTILISATEUR_DE_DEPART
-su $UTILISATEUR_DE_DEPART
+# su $UTILISATEUR_DE_DEPART
 cd $HOME
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+sudo mkdir -p /root/.kube
+sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
+sudo chown root:root /root/.kube/config
 # - Et maintenant, l'utilisateur $UTILISATEUR_DE_DEPART peut exécuter:
 # sudo kubectl get nodes
-#   # ou:
+#   # ou (pas encore) :
 # kubectl get nodes
 rm -f ./test-kubectl-get-nodes.resultat
 echo "# --------------------------------------------------------------------------------------------------- "  >> ./test-kubectl-get-nodes.resultat
@@ -34,7 +37,7 @@ echo "# server180627   NotReady   <none>    51m       v1.11.0"  >> ./test-kubect
 echo "# "  >> ./test-kubectl-get-nodes.resultat
 echo "# --------------------------------------------------------------------------------------------------- "  >> ./test-kubectl-get-nodes.resultat
 echo "# --------------------------------------------------------------------------------------------------- "  >> ./test-kubectl-get-nodes.resultat
-kubectl get nodes >> ./test-kubectl-get-nodes.resultat 2>&1
+sudo kubectl get nodes >> ./test-kubectl-get-nodes.resultat 2>&1
 echo "# --------------------------------------------------------------------------------------------------- "  >> ./test-kubectl-get-nodes.resultat
 echo "# 			FIN TEST CONFIG - KUBECTL"  >> ./test-kubectl-get-nodes.resultat
 echo "# si le test est concluant, vous devriez voir apparaître ci-dessous, une sortie standard de la forme:"  >> ./test-kubectl-get-nodes.resultat
